9a60a83e3546228622ce532713c768540aa46226
xaviergoh <13scxavier@gmail.com>
2018-04-08 14:59:43 +0800

    Commit 1

---
+++ assets/js/scripts.js
@@ -0,0 +1,249 @@
+/*
+Theme Name: IAMX
+Author: Ahmed Faruk
+Author URL: farukahmed.com
+*/
+
+/*
+    = Preloader
+    = Animated scrolling / Scroll Up
+    = Full Screen Slider
+    = Sticky Menu
+    = Back To Top
+    = Countup
+    = Progress Bar
+    = More skill
+    = Shuffle
+    = Magnific Popup
+*/
+
+jQuery(function ($) {
+
+    'use strict';
+
+    /* ---------------------------------------------- /*
+     * Preloader
+    /* ---------------------------------------------- */
+
+    $(window).ready(function() {
+        $('#pre-status').fadeOut('slow');
+        $('#tt-preloader').delay(400).fadeOut('slow');
+    });
+
+
+
+
+    // -------------------------------------------------------------
+    // Animated scrolling / Scroll Up
+    // -------------------------------------------------------------
+    (function () {
+        $('a[href*=#]').bind("click", function(e){
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top
+            }, 1000);
+            e.preventDefault();
+        });
+    }());
+
+
+
+    // -------------------------------------------------------------
+    // Full Screen Slider
+    // -------------------------------------------------------------
+    (function () {
+        $(".tt-fullHeight").height($(window).height());
+
+        $(window).resize(function(){
+            $(".tt-fullHeight").height($(window).height());
+        });
+
+    }());
+
+
+    // -------------------------------------------------------------
+    // Sticky Menu
+    // -------------------------------------------------------------
+    (function () {
+        $('.header').sticky({
+            topSpacing: 0
+        });
+
+        $('body').scrollspy({
+            target: '.navbar-custom',
+            offset: 70
+        })
+    }());
+
+    // -------------------------------------------------------------
+    // Fade in and out main menu at Full Screen Slider
+    // -------------------------------------------------------------
+    $(window).on('scroll', function() {
+ 		var h = $('section').height();
+ 		var y = $(window).scrollTop();
+    var nav = $('#nav-wrap');
+
+ 	   if ((y > h*.10) && (y < h) && ($(window).outerWidth() > 768 )) {
+ 	     nav.fadeOut('fast');
+ 	   } else if (y < h*.10) {
+       nav.removeClass('opaque').fadeIn('fast');
+     } else {
+       nav.addClass('opaque').fadeIn('fast');
+     }
+ 	});
+
+    // -------------------------------------------------------------
+    // Back To Top
+    // -------------------------------------------------------------
+
+    (function () {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('.scroll-up').fadeIn();
+            } else {
+                $('.scroll-up').fadeOut();
+            }
+        });
+    }());
+
+
+    // -------------------------------------------------------------
+    // Countup
+    // -------------------------------------------------------------
+    $('.count-wrap').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $(this).find('.timer').each(function () {
+                var $this = $(this);
+                $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+
+    // -------------------------------------------------------------
+    // Progress Bar
+    // -------------------------------------------------------------
+
+    $('.skill-progress').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $.each($('div.progress-bar'),function(){
+                $(this).css('width', $(this).attr('aria-valuenow')+'%');
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+    // -------------------------------------------------------------
+    // More skill
+    // -------------------------------------------------------------
+    $('.more-skill').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $('.chart').easyPieChart({
+                //your configuration goes here
+                easing: 'easeOut',
+                delay: 1000,
+                barColor:'#68c3a3',
+                trackColor:'rgba(255,255,255,0.2)',
+                scaleColor: false,
+                lineWidth: 8,
+                size: 140,
+                animate: 1500
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+
+    // -------------------------------------------------------------
+    // Shuffle
+    // -------------------------------------------------------------
+
+    (function () {
+
+        var $grid = $('#grid');
+
+        $grid.shuffle({
+            itemSelector: '.portfolio-item'
+        });
+
+        /* reshuffle when user clicks a filter item */
+        $('#filter a').click(function (e) {
+            e.preventDefault();
+
+            // set active class
+            $('#filter a').removeClass('active');
+            $(this).addClass('active');
+
+            // get group name from clicked item
+            var groupName = $(this).attr('data-group');
+
+            // reshuffle grid
+            $grid.shuffle('shuffle', groupName );
+        });
+
+
+    }());
+
+
+    // -------------------------------------------------------------
+    // Magnific Popup
+    // -------------------------------------------------------------
+
+    (function () {
+      $('.image-link').magnificPopup({
+
+        gallery: {
+          enabled: true
+        },
+        removalDelay: 300, // Delay in milliseconds before popup is removed
+        mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+        type:'image',
+        image: {
+            titleSrc: 'alt'
+        }
+
+      });
+
+    }());
+
+
+    // -------------------------------------------------------------
+    // STELLAR FOR BACKGROUND SCROLLING
+    // -------------------------------------------------------------
+
+    $(window).load(function() {
+
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+
+        }else {
+            $.stellar({
+                horizontalScrolling: false,
+                responsive: true
+            });
+        }
+
+    });
+
+
+    // -------------------------------------------------------------
+    // WOW JS
+    // -------------------------------------------------------------
+
+    (function () {
+
+        new WOW({
+
+            mobile:  false
+
+        }).init();
+
+    }());
+
+});
